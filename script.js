d52025650c280d6e8b756b4012a5b1bd7ac48b67
Irina Nicula <irina.nicula0@gmail.com>
2019-09-25 21:55:36 +0200

    Added method to reset player scores

--- script.js
+++ script.js
@@ -8,32 +8,60 @@ let characters = [
         score: 0
     },
     {
-        name: 'fin',
+        name: 'finn',
+        score: 0
+    },
+    {
+        name: 'PB',
         score: 0
     }
 ];
 
 let votes = document.querySelectorAll('input:checked');
 
-Array.from(votes).forEach(vote => {
-    // looop over characters and update the right character\s score
-    characters.forEach(character => {
-        if(vote.value == character.name) {
-            character.score++;
-        }
-    });
-})
-
-// loop over characters to find character with the highest score
+let submitButton = document.querySelector('.submit-button');
 
 let winner = {score: 0};
-characters.forEach(character => {
-    if(character.score > winner.score) {
-        winner = character;
-    }
+
+const resetScores = () => {
+    //reset score of each character
+    characters.forEach(character => {
+        character.score = 0;
+    });
+}
+
+const setCharacterScore = () => {
+    Array.from(votes).forEach(vote => {
+        // looop over characters and update the right character\s score
+        characters.filter(character => {
+            if(vote.value == character.name) {
+                character.score++;
+            }
+        });
+    });
+}
+
+const findWinner = () => {
+    //loop over characters to find character with the highest score
+    characters.find(character => {
+        if(character.score > winner.score) {
+            winner = character;
+        }
+    });
+    return winner;
+}
+
+const showResults = () => {
+    characters.forEach(character => {
+        if(winner.name == character.name) {
+            console.log(`Congratulations, your character is ${winner.name} with ${winner.score} points`);
+        }
+    });
+}
+
+submitButton.addEventListener('click', () => {
+    resetScores();
+    setCharacterScore();
+    findWinner();
+    showResults();
 });
-
-console.log(characters);
-
-console.log(winner)
-
